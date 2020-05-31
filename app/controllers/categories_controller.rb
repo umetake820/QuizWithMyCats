40032cedb02a44d20cd2a2f73c9b0757badc7830
umetake820 <umetac82@gmail.com>
2020-06-01 02:25:40 +0900

    commit

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def show
     category = Category.find(params[:id])
     gon.quizzes = category.quizzes.sample(5)
+    gon.images = Image.includes(:user).sample(5)
   end
 end
