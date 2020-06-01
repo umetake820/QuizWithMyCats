b351609ee5b4be79ed941bb860b7ba464c51d2fb
umetake820 <umetac82@gmail.com>
2020-06-01 10:56:09 +0900

    commit

--- db/seeds.rb
+++
@@ -1,4 +0,0 @@
-Category.create!([{ id: 1 }, { name: '学問' }])
-Category.create!([{ id: 2 }, { name: '生活' }])
-Category.create!([{ id: 3 }, { name: '芸能' }])
-Category.create!([{ id: 4 }, { name: 'スポーツ' }])
